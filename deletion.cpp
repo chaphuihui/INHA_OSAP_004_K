549498a8f3abbd230cd13ecdd2e0fa06261685ee
chaphuihui <167192501+chaphuihui@users.noreply.github.com>
2024-11-09 20:17:19 +0900

    add deletion.cpp and insertion.cpp
    add deletion.cpp and insertion.cpp

---
+++ deletion.cpp
@@ -0,0 +1,26 @@
+#include"AVLTree.h"
+#include"Utility.h"
+
+AVLNode* AVLTree::DeleteNode(AVLNode* root, int key) {
+    if (!root)
+        return root;
+
+    if (key < root->key)
+        root->left = DeleteNode(root->left, key);
+    else if (key > root->key)
+        root->right = DeleteNode(root->right, key);
+    else {
+        if (!root->left || !root->right) {
+            AVLNode* temp = root->left ? root->left : root->right;
+            if (!temp)
+                root = nullptr;
+            else
+                *root = *temp;
+            delete temp;
+        }
+        else {
+            AVLNode* temp = MinValueNode(root->right);
+            root->key = temp->key;
+            root->right = DeleteNode(root->right, temp->key);
+        }
+}
---
+++ insertion.cpp
@@ -0,0 +1,38 @@
+#include"AVLTree.h"
+#include"Utility.h"
+
+AVLNode* AVLTree::Insert(AVLNode* node, int key) {
+    if (!node)
+        return new AVLNode(key);
+
+    if (key < node->key)
+        node->left = Insert(node->left, key);
+    else if (key > node->key)
+        node->right = Insert(node->right, key);
+    else
+        return node;
+
+    node->height = UpdateHeight(node);
+
+    int balance = GetBalance(node);
+
+    if (balance > 1 && key < node->left->key)
+        return RightRotate(node);
+
+    if (balance < -1 && key > node->right->key)
+        return LeftRotate(node);
+
+    if (balance > 1 && key > node->left->key) {
+        node->left = LeftRotate(node->left);
+        return RightRotate(node);
+    }
+
+    if (balance < -1 && key < node->right->key) {
+        node->right = RightRotate(node->right);
+        return LeftRotate(node);
+    }
+
+    return node;
+};
+
+void AVLTree::Insert(int key) { root = Insert(root, key); };
