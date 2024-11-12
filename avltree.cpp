cd64a7d7b56c702063c571a6dd2d92212f9a31c1
minjun <miinjun0922@naver.com>
2024-11-12 12:33:01 +0900

    move Average function from avltree.cpp to normalfunction.cpp

--- avltree.cpp
+++ avltree.cpp
@@ -8,18 +8,3 @@ using namespace std;
 AVLTree::AVLTree() : root_(nullptr) {};
 AVLTree::~AVLTree() {}
 
-void AVLTree::Average(int key) {
-  AVLNode* subtree_root = Find(root_, key);
-  if (subtree_root == nullptr) {
-    cout << "Error: " << key << " is not in the AVL tree." << "\n";
-    return; 
-  }
-
-  int min_value_key = MinValueNode(subtree_root)->key;
-  int max_value_key = MaxValueNode(subtree_root)->key;
-
-  double subtree_min_max_average = (static_cast<double>(min_key) + static_cast<double>(max_key)) / 2.0;
-  cout << subtree_min_max_average << "\n";
-  
-}
-
--- normalfunction.cpp
+++ normalfunction.cpp
@@ -7,5 +7,20 @@ bool AVLTree::Empty() {};
 int AVLTree::Size() {};
 int AVLTree::Height() {};
 void AVLTree::Ancester() {};
-void AVLTree::Average() {};
+
+void AVLTree::Average(int key) {
+  AVLNode* subtree_root = Find(root_, key);
+  if (subtree_root == nullptr) {
+    cout << "Error: " << key << " is not in AVL tree." << "\n";
+    return;
+  }
+
+  int min_value_key = MinValueNode(subtree_root)->key;
+  int max_value_key = MaxValueNode(subtree_root)->key;
+
+  double subtree_min_max_average = (static_cast<double>(min_key) + static_cast<double>(max_key)) / 2.0;
+  cout << subtree_min_max_average << "\n";
+
+}
+
 bool AVLTree::Find(int key) {};
