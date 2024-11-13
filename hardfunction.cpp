651513a0de44a3fccf62a9d39714b0c729bcb0c0
Pak Denis <good.ice.cream7@gmail.com>
2024-11-13 23:50:33 +0900

    Update hardfunction.cpp
    Creating the Rank function

--- hardfunction.cpp
+++ hardfunction.cpp
@@ -2,5 +2,24 @@
 #include "utility.h"
 #include "deletion.cpp"
 
-void AVLTree::Rank() {};
+void AVLTree::Rank(AVLNode* node, int key) {
+   if (!node)
+   {
+       return 0;
+   }
+  
+   if (key < node->key)
+   {
+       return rank(node->left, key);
+   }
+   else if (key > node->key) 
+   {
+       return 1 + GetSize(node->left) + rank(node->right, key);
+   }
+   else 
+   {
+       return getSize(node->left) + 1;
+   }
+};
+
 void AVLTree::Erase(int key) { deleteNode(root, key); };
