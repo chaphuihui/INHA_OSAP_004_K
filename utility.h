ea6023b17a9b4f4afcfd71e2dd09f6d064ab0d2b
Pak Denis <good.ice.cream7@gmail.com>
2024-11-13 22:59:19 +0900

    Update utility.h
    Added:
    LeftRotate and RightRotate
    GetSize and UpdateSize

--- utility.h
+++ utility.h
@@ -6,10 +6,14 @@ public:
     Utility();
     ~Utility();
     int GetHeight(AVLNode* node);
+    int GetSeight(AVLNode* node);
     int UpdateHeight(AVLNode* node);
+    int UpdateSeight(AVLNode* node);
     int GetBalance(AVLNode* node);
     AVLNode* MinValueNode(AVLNode* root);
     AVLNode* MaxValueNode(AVLNode* root);
     AVLNode* FindNode(AVLNode* root, int key);
+    AVLNode* LeftRotate(AVLNode* x);
+    AVLNode* RightRotate(AVLNode* y);
 };
 #endif UTILITY_H_
