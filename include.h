0fe24fa31cc9262a99dcf10ed40f1b91493e3e88
chaphuihui <167192501+chaphuihui@users.noreply.github.com>
2024-11-16 22:31:51 +0900

    Add include.h
    Add include.h

---
+++ include.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+
+
+#include "iostream"
+#include "string"
+#include <iostream>
+#include <algorithm>
+
+
+#include "AVLtree.h"
+#include "Utility.h"
+
+
+
+using namespace std;
+
+
+#endif // !INCLUDE_H
